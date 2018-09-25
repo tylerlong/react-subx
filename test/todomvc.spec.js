ef868774ead8dae3d83e8f76d8f0bde75cf484cc
Tyler Long <tyler4long@gmail.com>
2018-09-26 07:26:54 +0800

    Test todomvc footer text

--- test/todomvc.spec.js
+++ test/todomvc.spec.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import TestRenderer from 'react-test-renderer'
 import SubX from 'subx'
+import delay from 'timeout-as-promise'
 
 import { Component } from '../src/index'
 
@@ -17,7 +18,7 @@ class App extends Component {
     const store = this.props.store
     return <div>
       <TodoForm addTodo={text => store.todos.push({ text, active: true })} />
-      { store.todos.map(todo => <TodoItem todo={todo} />) }
+      { store.todos.map(todo => <TodoItem todo={todo} key={todo.text} />) }
       <TodoFooter store={store} />
     </div>
   }
@@ -61,7 +62,7 @@ class TodoItem extends Component {
 class TodoFooter extends Component {
   render () {
     const store = this.props.store
-    return <div>
+    return <div id='footer'>
       {store.activeCount} Item{store.activeCount === 1 ? '' : 's'} left
     </div>
   }
@@ -75,6 +76,9 @@ describe('Markdown Editor', () => {
       activeCount: 0
     })
     const input = renderer.root.find(el => el.type === 'input')
+    const footer = renderer.root.find(el => el.props.id === 'footer')
+
+    console.log(footer.children)
 
     input.props.onChange({ target: { value: '111' } })
     expect(input.props.value).toBe('111')
@@ -87,6 +91,8 @@ describe('Markdown Editor', () => {
     })
     expect(input.props.value).toBe('')
 
+    console.log(footer.children)
+
     input.props.onChange({ target: { value: '222' } })
     expect(input.props.value).toBe('222')
     input.props.onKeyDown({ keyCode: 13, preventDefault: () => {} })
@@ -98,5 +104,26 @@ describe('Markdown Editor', () => {
       activeCount: 2
     })
     expect(input.props.value).toBe('')
+
+    console.log(footer.children)
+
+    input.props.onChange({ target: { value: '333' } })
+    expect(input.props.value).toBe('333')
+    input.props.onKeyDown({ keyCode: 13, preventDefault: () => {} })
+    expect(store).toEqual({
+      todos: [
+        { text: '111', active: true },
+        { text: '222', active: true },
+        { text: '333', active: true }
+      ],
+      activeCount: 3
+    })
+    expect(input.props.value).toBe('')
+
+    console.log(footer.children)
+
+    await delay(100)
+
+    console.log(footer.children)
   })
 })
