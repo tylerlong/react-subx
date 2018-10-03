0297eb9efcce059ea861affb0d98decb7ae173dd
Tyler Long <tyler4long@gmail.com>
2018-10-03 14:49:51 +0800

    Test force call super methods

---
+++ test/force_call_super.spec.js
@@ -0,0 +1,54 @@
+/* eslint-env jest */
+import React from 'react'
+
+describe('force call super', () => {
+  test('default 1', () => {
+    class A extends React.Component {
+    }
+    const a = new A()
+    expect(a.componentWillUnmount).toBeUndefined()
+  })
+  test('default 2', () => {
+    class A extends React.Component {
+    }
+    class B extends A {
+    }
+    const b = new B()
+    expect(b.componentWillUnmount).toBeUndefined()
+  })
+  test('default 3', () => {
+    let count = 0
+    class A extends React.Component {
+      constructor (props) {
+        super(props)
+        if (!this.componentWillUnmount) {
+          this.componentWillUnmount = () => {
+            count += 1
+          }
+        } else {
+          const temp = this.componentWillUnmount
+          this.componentWillUnmount = () => {
+            count += 1
+            temp()
+          }
+        }
+      }
+    }
+    class B extends A {
+    }
+    const b = new B()
+    expect(b.componentWillUnmount).toBeDefined()
+    expect(() => b.componentWillUnmount()).not.toThrow()
+    expect(count).toBe(1)
+
+    class C extends A {
+      componentWillUnmount () {
+        count += 1
+      }
+    }
+    const c = new C()
+    expect(c.componentWillUnmount).toBeDefined()
+    expect(() => c.componentWillUnmount()).not.toThrow()
+    expect(count).toBe(3)
+  })
+})
