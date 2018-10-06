3dd1423587d1537c7cd2963836fc4c9c9baf206e
Tyler Long <tyler4long@gmail.com>
2018-10-06 10:52:25 +0800

    Update test cases for update_props

--- test/update_props.spec.js
+++ test/update_props.spec.js
@@ -23,4 +23,58 @@ describe('update props', () => {
     const span = renderer.root.find(el => el.type === 'span')
     expect(span.children).toEqual(['world'])
   })
+
+  test('Class A & B 2', () => {
+    class A extends React.Component {
+      constructor (props) {
+        super(props)
+        const render = this.render.bind(this)
+        this.render = () => {
+          this.props = { text: 'world' }
+          return render()
+        }
+      }
+    }
+    class B extends A {
+      render () {
+        return <div>
+          <span>{this.props.text}</span>
+          <button onClick={e => { this.props.text = 'world 2'; this.forceUpdate() }} />
+        </div>
+      }
+    }
+    const renderer = TestRenderer.create(<B text='hello' />)
+    const span = renderer.root.find(el => el.type === 'span')
+    expect(span.children).toEqual(['world'])
+    const button = renderer.root.find(el => el.type === 'button')
+    button.props.onClick()
+    expect(span.children).toEqual(['world']) // update propsdoesn't work
+  })
+
+  test('Class A & B 3', () => {
+    class A extends React.Component {
+      constructor (props) {
+        super(props)
+        const render = this.render.bind(this)
+        this.render = () => {
+          this.props = { text: 'world' }
+          return render()
+        }
+      }
+    }
+    class B extends A {
+      render () {
+        return <div>
+          <span>{this.props.text}</span>
+          <button onClick={e => { this.props = { text: 'world 2' }; this.forceUpdate() }} />
+        </div>
+      }
+    }
+    const renderer = TestRenderer.create(<B text='hello' />)
+    const span = renderer.root.find(el => el.type === 'span')
+    expect(span.children).toEqual(['world'])
+    const button = renderer.root.find(el => el.type === 'button')
+    button.props.onClick()
+    expect(span.children).toEqual(['world']) // update propsdoesn't work
+  })
 })
