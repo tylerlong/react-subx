6da4046b08e8045e536d4df64215a3cfb0c70871
Tyler Long <tyler4long@gmail.com>
2018-10-06 09:46:05 +0800

    Write tests for assign to props

---
+++ test/assign_to_props.spec.js
@@ -0,0 +1,47 @@
+/* eslint-env jest */
+import React from 'react'
+import TestRenderer from 'react-test-renderer'
+
+describe('assign to props', () => {
+  test('assign to props doesn\'t work', async () => {
+    const historyNames = []
+    class A extends React.Component {
+      render () {
+        historyNames.push(this.props.person.firstName)
+        return <div>
+          {this.props.person.firstName}
+          <button onClick={e => {
+            this.props = { person: { firstName: 'Peter' } }
+            this.forceUpdate()
+          }}>Peter</button>
+        </div>
+      }
+    }
+
+    const renderer = TestRenderer.create(<A person={{ firstName: 'Tyler' }} />)
+    const button = renderer.root.find(el => el.type === 'button')
+    button.props.onClick()
+    expect(historyNames).toEqual(['Tyler', 'Tyler'])
+  })
+
+  test('assign to props\'s props does work', async () => {
+    const historyNames = []
+    class A extends React.Component {
+      render () {
+        historyNames.push(this.props.person.firstName)
+        return <div>
+          {this.props.person.firstName}
+          <button onClick={e => {
+            this.props.person.firstName = 'Peter'
+            this.forceUpdate()
+          }}>Peter</button>
+        </div>
+      }
+    }
+
+    const renderer = TestRenderer.create(<A person={{ firstName: 'Tyler' }} />)
+    const button = renderer.root.find(el => el.type === 'button')
+    button.props.onClick()
+    expect(historyNames).toEqual(['Tyler', 'Peter'])
+  })
+})
