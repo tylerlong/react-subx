796e7faadee4e20b85e82f4bd501925cb145cce0
Tyler Long <tyler4long@gmail.com>
2018-10-11 21:49:28 +0800

    Add recommended counter test case

---
+++ test/recommended_counter.spec.js
@@ -0,0 +1,47 @@
+/* eslint-env jest */
+import React from 'react'
+import TestRenderer from 'react-test-renderer'
+import SubX from 'subx'
+import delay from 'timeout-as-promise'
+
+import { Component } from '../src/index'
+
+const renderHisotry = []
+
+const counter = SubX.create({
+  number: 0,
+  decrease () {
+    this.number -= 1
+  },
+  increase () {
+    this.number += 1
+  }
+})
+
+class App extends Component {
+  render () {
+    const counter = this.props.counter
+    renderHisotry.push(counter.number)
+    return <div>
+      <button onClick={e => counter.decrease()}>-</button>
+      <span>{counter.number}</span>
+      <button onClick={e => counter.increase()}>+</button>
+    </div>
+  }
+}
+
+describe('Counter', () => {
+  test('default', async () => {
+    const renderer = TestRenderer.create(<App counter={counter} />)
+    const minusButton = renderer.root.find(el => el.type === 'button' && el.children && el.children[0] === '-')
+    const addButton = renderer.root.find(el => el.type === 'button' && el.children && el.children[0] === '+')
+    minusButton.props.onClick()
+    expect(counter.number).toBe(-1)
+    addButton.props.onClick()
+    expect(counter.number).toBe(0)
+    addButton.props.onClick()
+    expect(counter.number).toBe(1)
+    await delay(20)
+    expect(renderHisotry).toEqual([0, 1]) // Intermediate "-1, 0" were not rendered because of debounce
+  })
+})
