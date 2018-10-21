de416c8daf3c19043ea658974fbe51bd55c9cb21
Tyler Long <tyler4long@gmail.com>
2018-10-21 10:17:07 +0800

    Upgrade SubX

--- test/props_tracking.spec.js
+++ test/props_tracking.spec.js
@@ -20,9 +20,7 @@ describe('props tracking', () => {
     expect(R.map(R.dissoc('id'), events)).toEqual([ // only previously SubX Objects are tracked.
       {
         type: 'SET',
-        path: [ 'c', 'd', 'x' ],
-        val: 'world',
-        oldVal: undefined
+        path: [ 'c', 'd', 'x' ]
       }
     ])
   })
