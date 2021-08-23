8eb17bb2aa02d2f29d1fac3830637602a314eb66
Tyler Long <tyler4long@gmail.com>
2021-08-23 14:06:12 -0700

    Try to make jest work

--- test/jest.config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFiles: ['./setup.ts'],
-  testTimeout: 32000,
-};
-
-export default config;
--- test/setup.ts
+++ test/setup.ts
@@ -1,11 +1,12 @@
-let start;
+/* eslint-env jest */
+let start: number;
 
 beforeEach(() => {
-  start = new Date();
+  start = new Date().getTime();
 });
 
 afterEach(() => {
-  expect(new Date() - start).toBeLessThanOrEqual(
-    parseInt(process.env.BENCHMARK_THRESHOLD) || 100
+  expect(new Date().getTime() - start).toBeLessThanOrEqual(
+    parseInt(process.env.BENCHMARK_THRESHOLD ?? '100')
   );
 });
